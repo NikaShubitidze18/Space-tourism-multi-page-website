29524433685f29feb3bec409d315dc884268cd81
Nika_19 <nikashubitidze501@gmail.com>
2025-03-27 14:35:38 -0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-app/', // GitHub რეპოზიტორიის სახელი
+  base: '/Space-tourism-multi-page-website/', // GitHub რეპოზიტორიის სახელი
 })
