ffea932255835ee12097cbbf6e80581c8e7d55ea
Nika_19 <nikashubitidze501@gmail.com>
2025-03-27 14:00:08 -0700

    შეცვლა, ფაილი ან ფუნქციის დამატება

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Space-tourism-multi-page-website/', // შეცვალე შენი GitHub რეპოზიტორიის სახელით
+  base: '/Space-tourism-multi-page-website/', // GitHub რეპოზიტორიის სახელი
 })
